ac74f93d7493d1ba43f515955dc217c17da14957
NataliaMjasnikova <Natalim.Mjasnikova@medieinstitutet.se>
2025-10-01 11:30:50 +0200

    Update app.js
    green bottles

--- js/app.js
+++ js/app.js
@@ -11,3 +11,15 @@ console.log("This is line 6");
 console.log ("1.oktober");
 console.log ("natalim@live.se");
  */
+const text = "green bottles hanging on the wall";
+const text2 = "and if one green bottle, should accidentally fall, there will be";
+
+for (let i = 10; i>= 1; i--) {
+  const currentbottle = [i];
+  const nextbottle = [i-1];
+  console.log(currentbottle);
+  console.log(text);
+console.log(text2);
+}
+
+
