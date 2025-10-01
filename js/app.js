f3c23891a1a763d67b3f3ad5aad1d99052f5f305
NataliaMjasnikova <Natalim.Mjasnikova@medieinstitutet.se>
2025-10-01 10:15:12 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
-console.log(
-  "Hello world"
-
-);
+console.log("Hello world");
 console.log("Hello again");
+console.log("This is line 1.");
+console.log("This is line 2");
+console.log("This is line 3"); console.log("This is line 4");
+console.log("This is line 5");;
+;
+console.log("This is line 6");
