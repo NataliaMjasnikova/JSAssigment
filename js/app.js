3e2718c091a88caf4624b656495018061bec2a48
NataliaMjasnikova <Natalim.Mjasnikova@medieinstitutet.se>
2025-10-01 10:27:41 +0200

    Add comment

--- js/app.js
+++ js/app.js
@@ -6,3 +6,8 @@ console.log("This is line 3"); console.log("This is line 4");
 console.log("This is line 5");;
 ;
 console.log("This is line 6");
+
+/* console.log ("natalia");
+console.log ("1.oktober");
+console.log ("natalim@live.se");
+ */
